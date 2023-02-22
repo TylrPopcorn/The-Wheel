81fcb52d258805f4a4ddf2df751e5009b7c2f9ba
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-02-22 08:22:35 -0500

    ㅤ

---
+++ index.js
@@ -0,0 +1,28 @@
+//.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+//Styles:
+//import "../styles/reset.css";
+import "./styles/index.css";
+
+//code related imports:
+import App from "./src/App";
+
+/*
+  Dependencies:
+
+  npm install react
+  npm install parcel-bundler
+  npm install react-router-dom
+
+*/
+/*------------    -------------*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
