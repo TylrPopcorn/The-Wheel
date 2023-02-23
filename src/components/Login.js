835fdc03c19d27b0a71e870e967d3193d9294e59
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-02-23 07:11:48 -0500

    ㅤ

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Main function:
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  // ----------   ----------- \\
+
+  render() {
+    //Render items to the screen for the user to see:
+    return <div></div>;
+  }
+}
+
+export default Login;
