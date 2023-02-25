2bf5a2ae83a5b01653a8e03020cdb49eeb1d59dd
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-02-24 20:06:33 -0500

    ㅤ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login"; //Login page.
+//---------
+//Main function:
 function App() {
   return (
+    //html
     <div className="App">
-      <h2> Test </h2>
+      <button id="logout">Logout</button>
+      <h1>The Wheel</h1> {/* Title */}
+      <nav>
+        {/*Navigation*/}
+        <Link id="LOGIN_SCREEN" to="/">
+          Login
+        </Link>
+        <Link id="THE_WHEEL" to="/wheel">
+          Wheel
+        </Link>
+      </nav>
+      <p id="error">ERRROR</p>
+      <Routes>
+        {/* Routes */}
+        <Route path="/" element={<Login />} />
+
+        <Route
+          exact
+          path="wheel"
+          //element={}
+        />
+      </Routes>
     </div>
   );
 }
