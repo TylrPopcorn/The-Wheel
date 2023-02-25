d81b5f79eac0658357ff65ae238d050813e5c2d9
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-02-25 12:53:59 -0500

    ㅤㅤㅤㅤ

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-//Main function:
+//---Main function:
 class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -10,8 +10,31 @@ class Login extends React.Component {
 
   render() {
     //Render items to the screen for the user to see:
-    return <div></div>;
+    return (
+      <div>
+        <form id="loginForm">
+          <h2>Login</h2>
+
+          {/* ERROR */}
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input type="username" name="username" id="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" id="password" />
+          </div>
+
+          <button type="submit"> LOGIN </button>
+        </form>
+      </div>
+    );
   }
 }
+
+const adminLogin = {
+  username: "12345",
+  password: "54321",
+};
 
 export default Login;
