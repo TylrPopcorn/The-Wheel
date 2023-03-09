473b1b533a8aed665f726f1b1e622453388ad1d5
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-03-09 07:01:59 -0500

    ㅤㅤㅤㅤ

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Wheel = (props) => {
-  return <div></div>;
+  return (
+    <div className="WHEEL">
+      <div className="wheel-container">
+        <h2>Test</h2>
+        <h2>Test</h2>
+        <h2>Test</h2>
+        <h2>Test</h2>
+        <h2>Test</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Wheel;
