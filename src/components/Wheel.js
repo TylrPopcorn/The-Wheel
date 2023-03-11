e47a7c57e60657a4dc9e89fad0b416ec6132e88b
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-03-11 00:02:05 -0500

    Working on

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,14 +1,29 @@
 import React from "react";
 
+const wheel = 1;
+
 const Wheel = (props) => {
   return (
     <div className="WHEEL">
       <div className="wheel-container">
-        <h2>Test</h2>
-        <h2>Test</h2>
-        <h2>Test</h2>
-        <h2>Test</h2>
-        <h2>Test</h2>
+        <div id="wheel-physical">
+          {[0, 1, 2, 3, 4, 5].map((val) => (
+            <div
+              key={val}
+              className={`cog ${wheel === val ? "active" : ""}`}
+              style={{ "--i": val }}
+            >
+              {/* --i is a custom CSS property. */}
+            </div>
+          ))}
+        </div>
+
+        <p className="TM_text"> Times moved: 0</p>
+
+        <div id="keypad">
+          <button>{"-"}</button>
+          <button>{"+"}</button>
+        </div>
       </div>
     </div>
   );
