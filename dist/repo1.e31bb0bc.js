601caba08c784a10a37ebc2b888d56c49e9ce7fc
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-03-12 11:21:43 -0400

    ㅤㅤㅤㅤ

--- dist/repo1.e31bb0bc.js
+++ dist/repo1.e31bb0bc.js
@@ -35585,15 +35585,52 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const Wheel = props => {
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function Wheel(props) {
+  const [state, setData] = (0, _react.useState)({
+    TimesMoved: 0,
+    Wheel: 4
+  });
+  function onClick(option) {
+    const amount = option === "-" ? -1 : 1;
+    let newWheel = state.Wheel + amount;
+    if (newWheel < 0) {
+      newWheel = 5;
+    } else if (newWheel > 5) {
+      newWheel = 0;
+    }
+    setData({
+      ...state,
+      TimesMoved: state.TimesMoved + 1,
+      Wheel: newWheel
+    });
+  }
+
+  //render to the screen:
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "WHEEL"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "wheel-container"
-  }, /*#__PURE__*/_react.default.createElement("h2", null, "Test"), /*#__PURE__*/_react.default.createElement("h2", null, "Test"), /*#__PURE__*/_react.default.createElement("h2", null, "Test"), /*#__PURE__*/_react.default.createElement("h2", null, "Test"), /*#__PURE__*/_react.default.createElement("h2", null, "Test")));
-};
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    id: "wheel-physical"
+  }, [0, 1, 2, 3, 4, 5].map(val => /*#__PURE__*/_react.default.createElement("div", {
+    key: val,
+    className: `cog ${state.Wheel === val ? "active" : ""}`,
+    style: {
+      "--i": val
+    }
+  }))), /*#__PURE__*/_react.default.createElement("p", {
+    className: "TM_text"
+  }, `Times moved: ${state.TimesMoved}`), /*#__PURE__*/_react.default.createElement("div", {
+    id: "keypad"
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => onClick("-")
+  }, "-"), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => onClick("+")
+  }, "+"))));
+}
 var _default = Wheel;
 exports.default = _default;
 },{"react":"node_modules/react/index.js"}],"src/App.js":[function(require,module,exports) {
@@ -35727,7 +35764,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65454" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50448" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-const wheel = 1;
+function Wheel(props) {
+  const [state, setData] = useState({
+    TimesMoved: 0,
+    Wheel: 4,
+  });
 
-const Wheel = (props) => {
+  function onClick(option) {
+    const amount = option === "-" ? -1 : 1;
+    let newWheel = state.Wheel + amount;
+
+    if (newWheel < 0) {
+      newWheel = 5;
+    } else if (newWheel > 5) {
+      newWheel = 0;
+    }
+
+    setData({
+      ...state,
+      TimesMoved: state.TimesMoved + 1,
+      Wheel: newWheel,
+    });
+  }
+
+  //render to the screen:
   return (
     <div className="WHEEL">
       <div className="wheel-container">
+        {/* the actual wheel */}
         <div id="wheel-physical">
           {[0, 1, 2, 3, 4, 5].map((val) => (
             <div
               key={val}
-              className={`cog ${wheel === val ? "active" : ""}`}
+              className={`cog ${state.Wheel === val ? "active" : ""}`}
               style={{ "--i": val }}
             >
               {/* --i is a custom CSS property. */}
             </div>
           ))}
         </div>
+        {/*--------     -----------*/}
 
-        <p className="TM_text"> Times moved: 0</p>
+        <p className="TM_text">{`Times moved: ${state.TimesMoved}`}</p>
 
+        {/* KEYPAD */}
         <div id="keypad">
-          <button>{"-"}</button>
-          <button>{"+"}</button>
+          <button onClick={() => onClick("-")}>{"-"}</button>
+          <button onClick={() => onClick("+")}>{"+"}</button>
         </div>
+        {/*--------     -----------*/}
       </div>
     </div>
   );
-};
+}
 
 export default Wheel;
